b0554427a39977edc7288639ffd76c35e291d02c
seyyar95 <heydrovsyyar@gmail.com>
2024-04-29 20:06:34 +0400

    Concurrent coroutines

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ for a random delay between 0 and max_delay (included and float value)
 seconds and eventually returns it.
 """
 import asyncio
-from random import randrange
+from random import randint
 
 
 async def wait_random(max_delay: int = 10) -> float:
@@ -14,6 +14,6 @@ async def wait_random(max_delay: int = 10) -> float:
     asynchronous coroutine that takes in an integer argument
     and return the random seconds it waits
     """
-    wait = float(randrange(max_delay))
+    wait = float(randint(0, max_delay))
     await asyncio.sleep(wait)
     return wait
