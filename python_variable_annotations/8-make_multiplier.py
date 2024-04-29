d63ed8eaa1e1c17d07bbe34b9f3a44cd3a5130ad
seyyar95 <heydrovsyyar@gmail.com>
2024-04-29 15:14:05 +0400

    Let's duck type an iterable object

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -5,10 +5,10 @@ Module that defines a type-annotated funtion
 from typing import Callable
 
 
-def make_multiplier(multipler: float) -> Callable[[float], float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     Returns a function that multiplies a float by the multiplier.
     """
     def mul(x: float) -> float:
-        return x * multipler
+        return x * multiplier
     return mul
--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,7 +5,7 @@ Model that defines a type-annotated function element_length
 from typing import Iterable, List, Tuple, Sequence
 
 
-def element_length(lst: Iterable[str]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     Returns a list of integers representing the lengths of the strings in lst.
     """
