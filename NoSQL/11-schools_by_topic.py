d9712a9401bd3913966e5dc8f3342fb4c3ccae61
seyyar95 <heydrovsyyar@gmail.com>
2024-05-06 21:32:32 +0400

    Where cn I learn Python/

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Function that returns the list of school having a specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    return list of school having a specific topic
+    """
+    return mongo_collection.find({'topics': topic})
