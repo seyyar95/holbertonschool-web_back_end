18c637fe9d9b7391889c8f3764a2d2d06598d1b0
seyyar95 <heydrovsyyar@gmail.com>
2024-05-06 21:59:19 +0400

    Log stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -6,10 +6,6 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    """
-    provide stats about Nginx logs
-    """
-
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
@@ -18,7 +14,7 @@ if __name__ == "__main__":
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = collection.count_documents({'method': method})
-        print(f'\tmethod {method} {count}')
+        print(f'\tmethod {method}: {count}')
 
     status = collection.count_documents({'method': 'GET', 'path': '/status'})
     print(f'{status} status check')
