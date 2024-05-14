f9cf2a9b9dbf600949154876e0a7053ff2edea4d
seyyar95 <heydrovsyyar@gmail.com>
2024-05-14 18:50:55 +0400

    Evcar is ready but I am not

--- ES6_classes/100-evcar.js
+++ ES6_classes/100-evcar.js
@@ -1,13 +1,12 @@
 import Car from './10-car';
 
-export default class EVCar {
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
+    super(brand, motor, color);
     this._range = range;
   }
 
+  // eslint-disable-next-line class-methods-use-this
   cloneCar() {
     return new Car();
   }
