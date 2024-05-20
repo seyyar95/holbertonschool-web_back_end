8e054211b2f6ba314be89c7d6cd5b14596fae583
seyyar95 <heydrovsyyar@gmail.com>
2024-05-20 18:24:44 +0400

    why do you need to catch me?

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,3 @@
 export default function handleResponseFromAPI(promise) {
-  if (promise) {
-    promise.then(console.log('Got a response from the API'));
-    return ({ status: 200, body: 'success' });
-  }
-  return (new Error());
+  return promise.then(() => ({ status: 200, body: 'success' })).catch(() => new Error()).finally(() => console.log('Got a response from the API'));
 }
