8d856d77b8832ca8fbe3ccaaf0ada0a1df7d4b65
seyyar95 <heydrovsyyar@gmail.com>
2024-05-21 16:43:29 +0400

    They forced me to use map

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,5 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  return Promise.allSettled(promises).then((results) => results.forEach((result) => result));
+  return Promise.allSettled(promises).then((results) => (
+    results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : String(result.reason),
+    }))
+  ));
 }
