c0ac599dc798adde163ac7d01eb48a65c4b29e37
seyyar95 <heydrovsyyar@gmail.com>
2024-05-22 14:41:08 +0400

    I just forgot to throw error

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,5 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) throw new Error('Position outside range');
   const buffer = new ArrayBuffer(length);
   const dataview = new DataView(buffer);
   dataview.setInt8(position, value);
