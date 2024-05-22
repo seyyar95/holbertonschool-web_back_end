fe8ee6c2fc0279076b7fe730da1a9ff91ae6d01b
seyyar95 <heydrovsyyar@gmail.com>
2024-05-22 17:23:32 +0400

    Eslint error fixed

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,5 @@
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
   return map.forEach((value, key) => {
     if (value === 1) map.set(key, 100);
   });
